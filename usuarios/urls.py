21d0115c5386a546b730f9b26763d65d606c62fe
Leonardo Jordão Granata <granataleonardo27@gmail.com>
2025-09-21 13:24:03 -0300

    Adicionando rotas para página de cadastro, logout e listagem de usuários

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("login/", views.loginUser, name="login"),
     path("cadastro/", views.cadastroUser, name="cadastro"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("listar/", views.listarUsers, name="listar"),
 ]
