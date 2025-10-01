f70c15743b6cd975283d0e7ed2b42ba66ded7fd4
Leonardo <granataleonardo27@gmail.com>
2025-10-01 15:42:09 -0300

    Adicionando funcionalidade de cadastrar, editar e excluir para o estoque

--- estoque/urls.py
+++ estoque/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("cadastro/", views.cadastroItem, name="cadastroItem"),
+    path("editar/<int:pk>/", views.editarItem, name="editarItem"),
+    path("excluir/<int:pk>/", views.excluirItem, name="excluirItem"),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Estoque
+from .forms import EstoqueForm
+from django.shortcuts import get_object_or_404, redirect
 
+# CRUD
 
 @login_required
 def home(request):
-    return render(request, 'estoque/home.html')
+    itens = Estoque.objects.all()
+    return render(request, 'estoque/home.html', {'itens': itens})
 
 @login_required
 def cadastroItem(request):
@@ -13,9 +17,30 @@ def cadastroItem(request):
         form = EstoqueForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, "Item cadastrado com sucesso")
             return redirect('home')
     else:
         form = EstoqueForm()
-    return render(request, './estoque/cadastro.html', {'form': form})
+    return render(request, 'estoque/cadastro.html', {'form': form})
 
+@login_required
+def editarItem(request, pk):
+    estoque = get_object_or_404(Estoque, pk=pk)
+    if request.method == 'POST':
+        form = EstoqueForm(request.POST, instance=estoque)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  
+    else:
+        form = EstoqueForm(instance=estoque)
+    return render(request, 'estoque/editar.html', {'form': form, 'estoque': estoque})
+
+@login_required
+def excluirItem(request, pk):
+    estoque = get_object_or_404(Estoque, pk=pk)
+    if request.method == 'POST':
+        estoque.delete()
+        return redirect('home')
+    return render(request, 'estoque/excluir.html', {'estoque': estoque}) 
+
+
+# -----------------
