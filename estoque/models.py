6933929ec2a72d7a2b440ee918458e328a5fa384
Leonardo <granataleonardo27@gmail.com>
2025-10-01 14:29:46 -0300

    Criando o models.py

--- estoque/models.py
+++ estoque/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
+class Estoque(models.Model):
+    codigo = models.CharField(max_length=25)
+    descricao = models.TextField()
+    quantidade = models.IntegerField()
+    marca = models.CharField(max_length=120)
+    fornecedor = models.CharField(max_length=50)
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    qtd_min = models.IntegerField()
+    qtd_max = models.IntegerField()
 
-
+    def __str__(self):
+        return self.descricao
