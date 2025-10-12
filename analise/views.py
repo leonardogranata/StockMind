8b122ae0240683fbe17d0b39b866084028142b6f
Leonardo Jordão Granata <granataleonardo27@gmail.com>
2025-10-12 12:32:58 -0300

    Adicionando os gráficos

--- analise/views.py
+++ analise/views.py
@@ -1,17 +1,43 @@
+# Em analise/views.py
+
+import json
 from django.shortcuts import render
-from .ia import prever_consumo
 from estoque.models import Estoque
+
+# AQUI ESTÁ A CORREÇÃO: Importe a sua função!
+# Supondo que a função está em 'servicos_analise.py' no mesmo diretório
+from analise.ia import prever_consumo
+
+# Se o arquivo se chamasse utils.py, seria:
+# from .utils import prever_consumo
+
 
 def previsao(request):
     previsoes_itens = []
 
     itens = Estoque.objects.all()
     for item in itens:
-        # gera a previsão (últimos 15 dias)
-        previsao = prever_consumo(item.nome, 15)
+        # Agora o Python sabe o que é 'prever_consumo'
+        previsao_df = prever_consumo(item.nome, 15)
+
+        # ... (o resto do seu código continua igual)
+        if previsao_df is None or previsao_df.empty:
+            continue
+
+        previsao_df['ds'] = previsao_df['ds'].dt.strftime('%Y-%m-%d')
+        previsao_filtrada = previsao_df[['ds', 'yhat']].copy()
+        previsao_lista_para_json = previsao_filtrada.to_records(index=False).tolist()
+        previsao_em_json = json.dumps(previsao_lista_para_json)
+        previsao_para_tabela = previsao_filtrada.to_dict('records')
+
         previsoes_itens.append({
             'item': item.nome,
-            'previsao': previsao
+            'previsao_tabela': previsao_para_tabela,
+            'previsao_json': previsao_em_json
         })
 
-    return render(request, 'analise/previsao.html', {'previsoes_itens': previsoes_itens})
+    context = {
+        'previsoes_itens': previsoes_itens
+    }
+
+    return render(request, 'analise/previsao.html', context)
