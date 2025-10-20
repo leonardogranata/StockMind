bd15a0f9583998bbead57b732ab1215e7c702f76
Leonardo <granataleonardo27@gmail.com>
2025-10-20 15:18:00 -0300

    Adicionando as rotas para a página de itens mais usados e dashboard

--- analise/urls.py
+++ analise/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'), 
     path('previsao/', views.previsao, name='previsao'),
+    path('mais-usados/', views.itens_mais_usados, name='mais_usados'),
 ]
